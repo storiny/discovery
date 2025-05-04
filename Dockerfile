373066162f6f0eea7234d9d5343acda5b659de62
zignis <velociraptorm77@gmail.com>
2025-05-04 18:09:17 +0530

    fix env

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Start with a rust alpine image
-FROM rust:alpine3.18
+FROM rust:1.86-alpine3.21
 # This is important, see https://github.com/rust-lang/docker-rust/issues/85
 ENV RUSTFLAGS="-C target-feature=-crt-static"
 # if needed, add additional dependencies here
@@ -12,7 +12,7 @@ RUN cargo build --release
 RUN strip target/release/storiny_discovery
 
 # use a plain alpine image, the alpine version needs to match the builder
-FROM alpine:3.18
+FROM alpine:3.21
 # if needed, install additional dependencies here
 RUN apk add --no-cache libgcc
 # copy the binary into the final image
--- src/providers/providers.rs
+++ src/providers/providers.rs
@@ -1,20 +1,24 @@
 use crate::spec::Provider;
 use lazy_static::lazy_static;
-use load_dotenv::load_dotenv;
 use regex::Regex;
 use url::Url;
 
-load_dotenv!();
-
 lazy_static! {
     pub static ref PROVIDERS: Vec<Provider> = {
-        let is_dev = option_env!("IS_DEV")
-            .map(|val| val == "true")
-            .unwrap_or_default();
         let json_data = include_bytes!("./providers.json");
         #[allow(clippy::expect_used)]
         let mut providers: Vec<Provider> =
             serde_json::from_slice(json_data).expect("failed to read providers from the JSON file");
+
+        // Base for stylesheets
+        #[cfg(debug_assertions)]
+        let base = format!(
+            "http://{}:{}",
+            std::env::var("HOST").unwrap(),
+            std::env::var("PORT").unwrap()
+        );
+        #[cfg(not(debug_assertions))]
+        let base = "https://discovery.storiny.com".to_string();
 
         // Convert schemas to regex matchers for each provider.
         providers.iter_mut().for_each(|provider| {
@@ -37,14 +41,7 @@ lazy_static! {
             for stylesheet in provider.stylesheets.iter_mut() {
                 if Url::parse(stylesheet).is_err() {
                     // Add base to relative URL.
-                    *stylesheet = format!(
-                        "{}/vendor/stylesheets/{stylesheet}",
-                        if is_dev {
-                            format!("http://{}:{}", env!("HOST"), env!("PORT"))
-                        } else {
-                            "https://discovery.storiny.com".to_string()
-                        }
-                    )
+                    *stylesheet = format!("{base}/vendor/stylesheets/{stylesheet}",)
                 }
             }
         });
